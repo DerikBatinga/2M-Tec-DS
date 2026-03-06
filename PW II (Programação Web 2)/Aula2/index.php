0465588479f63cb5311f5ee453672b169ce30467
SrDerik <deriksoares217@gmail.com>
2026-03-06 00:04:55 -0300

    new:Exercicio aula 2

--- PW II (Programação Web 2)/Aula2/index.php
+++ PW II (Programação Web 2)/Aula2/index.php
@@ -6,32 +6,56 @@
     <title>Aula 2</title>
 
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Changa+One:ital@0;1&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Playpen+Sans:wght@100..800&display=swap');
         body{
             position: relative;
-            background-image: url(img/alonso.gif);
+            
+            background: 
+            linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
+            url(img/alonso.gif) no-repeat;
+            background-size: 150rem;
+
+            height: 100vh;           /* altura da tela */
+            display: flex;           /* ativa flexbox */
+            justify-content: center; /* centraliza horizontal */
+            align-items: center;     /* centraliza vertical */
+    
         }
         .texto{
-            font-size: 20px;
-            font-family: 'Montserrat';
-            align-items: center;
-            justify-content: center;
+            color: #c0c6ce;
+            font-size: 30px;
+            font-family: "playpen Sans";
+            background:  #010011;
+            border-radius: 5%;
+            border: solid rgb(54, 154, 216) ;
+        }
+        .texto img{
+            width: 30px;
+        }
+        .texto a{
+            color: yellow;
+            text-decoration: none;
+        }
+        .texto a:hover{
+            color:orange;
         }
     </style>
 </head>
-<body>
-<div class="texto">
+<body>       
+    <div class="texto">
     <?php
     $nome = "Derik";
     $sobrenome = "Batinga";
     $idade = 16;
-
+    
     $data_nascimento = "11/08/2009";
-
+    
     echo"<p>Meu nome é:$nome $sobrenome</p>";
     echo"<p>Tenho $idade anos</p>";
     echo "<p> Nasci em $data_nascimento";
     ?>
+    <br></br>
+    <img src="img/github.webp" alt="MeuGit"><a href="https://github.com/DerikBatinga">Meu GitHub</a>
 </div>
 </body>
 </html>
