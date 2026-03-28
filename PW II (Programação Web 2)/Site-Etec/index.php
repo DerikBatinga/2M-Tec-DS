98f47615c8a68ae9c8fb298aca5fd79b839f7807
SrDerik <deriksoares217@gmail.com>
2026-03-28 20:43:35 -0300

    new: Recriação do site da Etec

---
+++ PW II (Programação Web 2)/Site-Etec/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>EtecZL-Início</title>
+</head>
+<body>
+    <header class="header">
+      <img class="logo_nav" src="img/EtecZL.svg" alt="Logo EtecZL">  
+        <nav>
+            <div class="container_nav">
+                <ul class="lista_nav">
+                    <li>
+                        <i class="ri-home-2-fill"></i>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <i class="ri-graduation-cap-fill"></i>
+                        <a href="Cursos/cursos.php">Cursos</a>
+                    </li>
+                    <li>
+                        <i class="ri-team-fill"></i>
+                        <a href="Gestao/gestao.php">Gestão</a>
+                    </li>
+                    <li>
+                        <i class="ri-phone-fill"></i>    
+                        <a href="Contato/contato.php">Contato</a>
+                    </li>
+                    <li>
+                        <a class="btn_nav" href="contato.php">Prestar prova</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <section class="hero">
+        <img src="img/Etec.webp" alt="Hero">
+        <h1>Etec da Zona Leste</h1>
+        <p>Venha estudar na melhor Etec da zona leste de São Paulo!</p>
+        <button class="botao_estilo hero_botao">Fazer vestibulinho</button>
+    </section>
+
+    <section>
+       <h2>Destaques e Premiações</h2>
+        <div class="card">
+            <img src="" alt="">
+            <h3>2º Lugar na Feteps</h3>
+            <p>Projeto do AMS conquista 2º lugar na Feteps com projeto inovador!</p>
+        </div>
+
+     <div class="card">
+            <img src="" alt="">
+            <h3>Novo Email Instucional</h3>
+            <p>A Etec está com um novo email institucional, consulte o seu NSA para mais informações</p>
+     </div>
+
+     <div class="card">
+            <img src="" alt="">
+            <h3>Da Etec direto para Havard!</h3>
+            <p>3º Aulão de inglês de Havard já está disponível em seu EMAIL!</p>
+     </div>
+
+     <div class="card">
+            <img src="" alt="">
+            <h3>Aluna aprovada na USP</h3>
+            <p>A estudante de administração Viviane Cardoso foi aceito em medicina na USP!</p>
+     </div>
+    </section>
+
+    <section class="links_uteis">
+       <div class="card">
+         <img src="" alt="">
+         <h3>NSA</h3>
+         <p>Sistema estudantiu</p>
+       </div>
+
+        <div class="card">
+         <img src="" alt="">
+         <h3>Email institucional</h3>
+         <p>Acesse agora por meio do Outlook</p>
+       </div>
+
+        <div class="card">
+         <img src="" alt="">
+         <h3>Vestibulinho</h3>
+         <p>Canditate-se agora para ter a oportunidade da sua vida!</p>
+       </div>
+
+        <div class="card">
+         <img src="" alt="">
+         <h3>Contato</h3>
+         <p>Entre em contato agora</p>
+       </div>
+    </section>
+
+    <footer class="">
+        <article>
+            <span>Etec da Zona Leste- Cada vez se superando!</span>
+            <i class="ri-instagram-fill"></i>
+            <i class="ri-youtube-fill"></i>
+            <i class="ri-facebook-box-fill"></i>
+            <i class="ri-phone-fill"></i>
+        </article>
+            
+      <span>&copy;Etec da Zona Leste- Derik Batinga</span>
+    </footer>
+        
+</body>
+</html>
