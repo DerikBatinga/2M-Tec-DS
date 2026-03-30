5efbc160aefe0f912b34ca2c3a8d415c27fdd938
Derik Batinga <deriksoares217@gmail.com>
2026-03-29 23:59:32 -0300

    Development: Desenvolvendo página inicial

--- PW II (Programação Web 2)/Site-Etec/index.php
+++ PW II (Programação Web 2)/Site-Etec/index.php
@@ -4,32 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet">
     <title>EtecZL-Início</title>
 </head>
 <body>
     <header class="header">
-      <img class="logo_nav" src="img/EtecZL.svg" alt="Logo EtecZL">  
+      <img class="logo_nav" src="img/EtecZL.png" alt="Logo EtecZL">  
         <nav>
             <div class="container_nav">
                 <ul class="lista_nav">
-                    <li>
-                        <i class="ri-home-2-fill"></i>
-                        <a href="#">Home</a>
+                    <li>               
+                        <a href="#"> <i class="ri-home-2-fill"></i>Home</a>
+                    </li>
+                    <li>                
+                        <a href="Cursos/cursos.php"> <i class="ri-graduation-cap-fill"></i>Cursos</a>
+                    </li>
+                    <li>               
+                        <a href="Gestao/gestao.php"> <i class="ri-team-fill"></i>Gestão</a>
                     </li>
                     <li>
-                        <i class="ri-graduation-cap-fill"></i>
-                        <a href="Cursos/cursos.php">Cursos</a>
+                        <a href="Contato/contato.php"> <i class="ri-phone-fill"></i>    Contato</a>
                     </li>
                     <li>
-                        <i class="ri-team-fill"></i>
-                        <a href="Gestao/gestao.php">Gestão</a>
-                    </li>
-                    <li>
-                        <i class="ri-phone-fill"></i>    
-                        <a href="Contato/contato.php">Contato</a>
-                    </li>
-                    <li>
-                        <a class="btn_nav" href="contato.php">Prestar prova</a>
+                        <a class="btn" href="contato.php">Prestar prova</a>
                     </li>
                 </ul>
             </div>
@@ -37,33 +34,40 @@
     </header>
 
     <section class="hero">
-        <img src="img/Etec.webp" alt="Hero">
         <h1>Etec da Zona Leste</h1>
         <p>Venha estudar na melhor Etec da zona leste de São Paulo!</p>
-        <button class="botao_estilo hero_botao">Fazer vestibulinho</button>
+        <a class="btn">Fazer vestibulinho</a>
     </section>
 
     <section>
-        <img src="" alt="">
-        <h2>Historia da ZL</h2>
-        <p>
-            Escolher a Etec Zona Leste é optar por uma formação que une tradição e inovação. Localizada em um dos pontos mais estratégicos da região, nossa unidade é referência no Centro Paula Souza pela infraestrutura moderna e pelo corpo docente altamente qualificado.
-        </p>
-        <p>
-            Aqui, o aluno não apenas aprende a teoria, mas vivencia a prática em laboratórios de ponta, preparando-se para os desafios reais do mercado de trabalho. Além do ensino técnico gratuito de excelência, oferecemos um ambiente que fomenta o empreendedorismo, a ética e o desenvolvimento humano. Estudar na Etec ZL é mais do que obter um diploma; é construir uma rede de contatos e abrir portas para as melhores oportunidades de carreira e universidades.
-        </p>
-    </section>
+        <div class="container">
+            <img src="img/Historia.webp" alt="">
+            <div class="container_titulotexto">
 
+                <h2>Historia da ZL</h2>
+                <p>
+                    A história da Etec Zona Leste se confunde com o desenvolvimento educacional da região leste de São Paulo. Fundada com o objetivo de suprir a carência de mão de obra especializada em setores tecnológicos e administrativos, a unidade nasceu do compromisso do Governo do Estado com a democratização do ensino técnico de qualidade.
+                </p>
+                <p>
+                    Desde a sua inauguração, a escola passou por diversas expansões, modernizando seus currículos para acompanhar a evolução digital. O que começou com algumas salas de aula transformou-se em um complexo educacional robusto, que hoje atende milhares de alunos em três períodos. Ao longo das décadas, orgulhamo-nos de ter formado profissionais que hoje lideram empresas, criam suas próprias startups e contribuem ativamente para o crescimento econômico e social da nossa comunidade. Nossa trajetória é marcada pela superação e pela certeza de que a educação é a ferramenta mais poderosa de transformação social.
+                </p>
+            </div>
+        </div>
+    </section>
+    
     <section> 
-        <h2>Porquê escolher está Etec?</h2>
-        <p>
-            A história da Etec Zona Leste se confunde com o desenvolvimento educacional da região leste de São Paulo. Fundada com o objetivo de suprir a carência de mão de obra especializada em setores tecnológicos e administrativos, a unidade nasceu do compromisso do Governo do Estado com a democratização do ensino técnico de qualidade.
-        </p>
-        <p>
-            Desde a sua inauguração, a escola passou por diversas expansões, modernizando seus currículos para acompanhar a evolução digital. O que começou com algumas salas de aula transformou-se em um complexo educacional robusto, que hoje atende milhares de alunos em três períodos. Ao longo das décadas, orgulhamo-nos de ter formado profissionais que hoje lideram empresas, criam suas próprias startups e contribuem ativamente para o crescimento econômico e social da nossa comunidade. Nossa trajetória é marcada pela superação e pela certeza de que a educação é a ferramenta mais poderosa de transformação social.
-        </p>
-    </h2>
-        <img src="" alt="">
+        <div class="container">
+             <div class="container_titulotexto">
+                 <h2>Porquê escolher está Etec?</h2>
+                 <p>
+                     Escolher a Etec Zona Leste é optar por uma formação que une tradição e inovação. Localizada em um dos pontos mais estratégicos da região, nossa unidade é referência no Centro Paula Souza pela infraestrutura moderna e pelo corpo docente altamente qualificado.
+                    </p>
+                    <p>
+                        Aqui, o aluno não apenas aprende a teoria, mas vivencia a prática em laboratórios de ponta, preparando-se para os desafios reais do mercado de trabalho. Além do ensino técnico gratuito de excelência, oferecemos um ambiente que fomenta o empreendedorismo, a ética e o desenvolvimento humano. Estudar na Etec ZL é mais do que obter um diploma; é construir uma rede de contatos e abrir portas para as melhores oportunidades de carreira e universidades.
+                    </p>
+                </div>
+            <img src="img/Estudantes.webp" alt="">
+        </div>
     </section>
     
     <section>
@@ -97,7 +101,7 @@
             <div class="card">
                 <i class="ri-database-2-fill"></i>
                 <h3>NSA</h3>
-                <p>Sistema estudantiu</p>
+                <p>Sistema estudantil</p>
             </div>
         
             <div class="card">
@@ -120,16 +124,52 @@
         </div>
     </section>
 
-    <footer class="">
-        <article>
-            <span>Etec da Zona Leste- Cada vez se superando!</span>
-            <i class="ri-instagram-fill"></i>
-            <i class="ri-youtube-fill"></i>
-            <i class="ri-facebook-box-fill"></i>
-            <i class="ri-phone-fill"></i>
-        </article>
-            
-      <span>&copy;Etec da Zona Leste- Derik Batinga</span>
+    <footer>
+        <div class="footer">
+            <div class="footer_col">
+                <img src="img/EtecZL.png" alt="">
+            <p>Etec da Zona Leste- Cada vez se superando!</p>
+            <ul>
+            <li>
+                <a href=""><i class="ri-instagram-fill"></i>Instagram</a>
+            </li>
+            <li>
+                <a href=""><i class="ri-youtube-fill"></i>Youtube</a>
+            </li>
+            <li>
+                <a href=""><i class="ri-facebook-box-fill"></i>Facebook</a>
+            </li>
+            <li>
+                <a href=""> <i class="ri-phone-fill"></i>Telefone</a>
+            </li>
+            </ul>
+        </div>
+        <div class="footer_col">
+            <p>Links rápidos</p>
+               <ul>
+                    <li>               
+                        <a href="#"> <i class="ri-home-2-fill"></i>Home</a>
+                    </li>
+                    <li>                
+                        <a href="Cursos/cursos.php"> <i class="ri-graduation-cap-fill"></i>Cursos</a>
+                    </li>
+                    <li>               
+                        <a href="Gestao/gestao.php"> <i class="ri-team-fill"></i>Gestão</a>
+                    </li>
+                    <li>
+                        <a href="Contato/contato.php"> <i class="ri-phone-fill"></i>    Contato</a>
+                    </li>
+    </div>
+        <div class="footer_col">
+            <p>Receba novidades</p>
+            <label for="email_footer">Email</label>
+            <input type="email" name="email" id="email_footer" placeholder="Seu e-mail aqui">
+            <button type="submit"><i class="ri-mail-line"></i></button>
+        </div>
+        <div class="footer_direitos">
+            <p>&copy;Etec da Zona Leste- Derik Batinga</p>
+            </div>
+        </div>
     </footer>
         
 </body>
